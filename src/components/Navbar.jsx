110a7262ec05b6f8edbb3952811abd516e8c075c
Victor Lascares <vmlascares@gmail.com>
2022-03-18 23:35:19 -0600

    Agregando estilos generales y componente navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,16 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-            <Link to="/works">Works</Link>
+        <div className="navbar">
+            <Link to="/">
+                <img src="/src/assets/img/Logo.svg" alt="Logo" />
+            </Link>
+            <nav className="nav">
+                <Link to="/">Home</Link>
+                <Link to="#about">About</Link>
+                <Link to="/works">Works</Link>
+                <Link to="#contact">Contact</Link>
+            </nav>
         </div>
     );
 };
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Works = () => {
     return (
         <div>
-            Works
+            <Navbar />
         </div>
     )
 }
