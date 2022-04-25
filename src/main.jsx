270dc47a9495d2addf89cb06462380608131dbd3
Victor Lascares <vmlascares@gmail.com>
2022-04-25 13:48:26 -0500

    Actualizando ubicacion de imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './css/normalize.css'
 import './css/index.css'
 import App from './components/App'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 ReactDOM.render(
   <React.StrictMode>
